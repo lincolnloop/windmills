7ba739f5a8cda5d43f65a7362dff1a5c45d46917
Yann Malet <yann.malet@gmail.com>
2011-09-04 20:03:42 +0200

    Improve the poratbility of the uwsgi_start.sh

--- perfo_analysis/bin/uwsgi_start.sh
+++ perfo_analysis/bin/uwsgi_start.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-export DJANGO_SETTINGS_MODULE=perfo_analysis.conf.local.settings
-
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-cd "$SCRIPT_DIR/../.."
-
-uwsgi --home $VIRTUAL_ENV \
-    --http :8084 \
+uwsgi \
     --master \
     --processes 4 \
+    --home $VIRTUAL_ENV \
+    --chdir $VIRTUAL_ENV/bin \
+    --env DJANGO_SETTINGS_MODULE=perfo_analysis.conf.local.settings \
+    --http :8084 \
     --module "django.core.handlers.wsgi:WSGIHandler()" \
     --static-map /static=$VIRTUAL_ENV/var/static
