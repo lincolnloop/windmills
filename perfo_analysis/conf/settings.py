29b52dab5ccb1950b2c7f83e7cc727a7ed656dd8
bkonkle <brandon@konkle.us>
2011-09-06 14:06:03 -0700

    Revise VAR_ROOT

--- perfo_analysis/conf/settings.py
+++ perfo_analysis/conf/settings.py
@@ -28,18 +28,9 @@ PROJECT_DIR, PROJECT_MODULE_NAME = os.path.split(
     os.path.dirname(os.path.realpath(perfo_analysis.__file__))
 )
 
-PYTHON_BIN = os.path.dirname(sys.executable)
-if os.path.exists(os.path.join(PYTHON_BIN, 'activate_this.py')):
-    # Assume that the presence of 'activate_this.py' in the python bin/
-    # directory means that we're running in a virtual environment. Set the
-    # variable root to $VIRTUALENV/var.
-    VAR_ROOT = os.path.join(os.path.dirname(PYTHON_BIN), 'var')
-    if not os.path.exists(VAR_ROOT):
-        os.mkdir(VAR_ROOT)
-else:
-    # Set the variable root to the local configuration location (which is
-    # ignored by the repository).
-    VAR_ROOT = os.path.join(PROJECT_DIR, PROJECT_MODULE_NAME, 'conf', 'local')
+VAR_ROOT = os.path.join(os.environ['VIRTUAL_ENV'], 'var')
+if not os.path.exists(VAR_ROOT):
+    os.mkdir(VAR_ROOT)
 
 #==============================================================================
 # Project URLS and media settings
